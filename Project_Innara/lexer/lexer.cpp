c9adafa226a246e80f4320607e942fa031190e53
ZEREF <79123274+MohammadHabibeh@users.noreply.github.com>
2026-03-05 06:13:45 -0500

    Add files via upload

---
+++ Project_Innara/lexer/lexer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#langauge example-> 5121235*238947
+typedef enum {
+	ADD;
+	MULTI;
+	MINUS;
+	DIV;
+}OP_TYPE;
+
+int OP(char &ch){
+	#set of all chs = [*, +, -, /]
+	switch(*ch){
+		case '*':
+			return OP -> ADD
+			break;
+		case '+':
+			return 
+			break;
+		case '-':
+			return true
+			break;
+		case '/':
+			return true;
+			break;
+	}	
+	return false;
+}
+
+
+int main(){
+
+	return 0;
+}
+
+
+
